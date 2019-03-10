848b096426077cce91c6dfa5f431b6513a48ced6
Kleiton Marques <kleiton.88design@gmail.com>
2019-03-10 08:10:40 -0300

    commite inicial

---
+++ src/app/05-event-emitters/vote.component.spec.ts
@@ -0,0 +1,19 @@
+import { VoteComponent } from './vote.component'; 
+
+describe('VoteComponent', () => {
+  var component: VoteComponent; 
+
+  beforeEach(() => {
+    component = new VoteComponent();
+  });
+
+  it('shold raise voteChanged event when upvoted', () => {
+  	let totalVotes = null;
+  	component.voteChanged.subscribe(tv => totalVotes = tv);
+
+  	component.upVote();
+
+  	// expect(totalVotes).not.toBeNull();
+  	expect(totalVotes).toBe(1);
+  });
+});
